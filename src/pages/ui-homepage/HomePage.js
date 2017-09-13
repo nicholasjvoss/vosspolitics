ef228f08501d146c95224e2a896f0b5d88bb6ae5
Nick <nicholasjvossjvoss@gmail.com>
2017-09-13 08:40:04 -0400

    added google civic information api to homepage

--- src/pages/ui-homepage/HomePage.js
+++ src/pages/ui-homepage/HomePage.js
@@ -1,40 +1,23 @@
 import React, { Component } from 'react';
-import { observer } from 'mobx-react';
+import { civicInfoApiKey } from '../../secret/APIKeys';
 
 // Components
 import Button from '../../components/scripts/Button.js';
 
 export default class HomePage extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        coords: {},
-        geoEnabled: false,
-      }
-  }
-
-  componentWillMount() {
-    navigator.geolocation.getCurrentPosition(this.showPosition.bind(this), this.geoNotAvailable.bind(this));
-  }
-
   render() {
-    const { coords, geoEnabled } = this.state;
-
     return (
       <section className="homepage">
         <main className="nv-main">
           <div className="masthead">
             <h1>Your representatives work for <i>you!</i></h1>
 
-            { geoEnabled ?
-              <Button
-                action={ this.handleFindRepDidClick.bind(this, coords) }
-                buttonType={ 1 }>
-                See what they're up to <span className="p-icon-chevron-right" />
-              </Button> :
-
-              <span>zip here instead...</span>
-            }
+            <input type="text" placeholder="Address" />
+            <Button
+              action={ ()=>{console.log('clicked')} }
+              buttonType={ 1 }>
+              See what they're up to <span className="p-icon-chevron-right" />
+            </Button>
 
           </div>
         </main>
@@ -42,17 +25,20 @@ export default class HomePage extends Component {
     );
   }
 
-  showPosition(pos) {
-    console.log('showPosition fired');
-    this.setState({ coords: pos.coords, geoEnabled: true });
+  // ===== api call =====
+
+  componentDidMount() {
+    const myRequest = new Request('https://www.googleapis.com/civicinfo/v2/representatives?address=5925+Weddington+Dr&includeOffices=true&levels=country&key=' + civicInfoApiKey);
+    const myInit = {
+      method: 'GET',
+      cache: 'default',
+    };
+
+    fetch(myRequest, myInit).then((response)=> {
+      let test = response;
+      console.log(test);
+    });
   }
 
-  geoNotAvailable() {
-    this.setState({ geoEnabled: false })
-  }
-
-  handleFindRepDidClick(coords) {
-    const { latitude, longitude } = coords;
-    console.log(coords);
-  }
+  // ==========
 }
